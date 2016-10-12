5c757e5a762dfcb08e0cd5cc32f88c0e2d4618ac
tianyun chen <tianyun chen>
2016-10-11 17:24:02 -0700

    Add:server set up

---
+++ EducationSystemBackEnd/src/main/java/com/education/controller/TeacherController.java
@@ -0,0 +1,21 @@
+package com.education.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api")
+public class TeacherController {
+
+	@RequestMapping("test")
+	public void tester(HttpServletRequest request,HttpServletResponse response) throws IOException{
+		String tester = request.getParameter("test");
+		response.getWriter().write("Server set up successfully!");
+		response.getWriter().close();
+	}
+}
