3ec4a93e77105dd097785439f8cc817d4450c038
tianyun chen <tianyun chen>
2016-10-17 22:59:12 -0700

    Add: signUpForTeacher

---
+++ EducationSystemBackEnd/src/main/java/com/education/bean/Teacher.java
@@ -0,0 +1,69 @@
+package com.education.bean;
+
+public class Teacher {
+   private int tid;
+   private String name;
+   private String password;
+   private String mobile;
+   private int status;
+   private int classAdviser;
+   private String createTime;
+   private String updateTime;
+public int getTid() {
+	return tid;
+}
+public void setTid(int tid) {
+	this.tid = tid;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getMobile() {
+	return mobile;
+}
+public void setMobile(String mobile) {
+	this.mobile = mobile;
+}
+public int getStatus() {
+	return status;
+}
+public void setStatus(int status) {
+	this.status = status;
+}
+public int getClassAdviser() {
+	return classAdviser;
+}
+public void setClassAdviser(int classAdviser) {
+	this.classAdviser = classAdviser;
+}
+public String getCreateTime() {
+	return createTime;
+}
+public void setCreateTime(String createTime) {
+	this.createTime = createTime;
+}
+public String getUpdateTime() {
+	return updateTime;
+}
+public void setUpdateTime(String updateTime) {
+	this.updateTime = updateTime;
+}
+@Override
+public String toString() {
+	return "Teacher [tid=" + tid + ", name=" + name + ", password=" + password + ", mobile=" + mobile + ", status="
+			+ status + ", classAdviser=" + classAdviser + ", createTime=" + createTime + ", updateTime=" + updateTime
+			+ "]";
+}
+   
+
+
+}
---
+++ EducationSystemBackEnd/src/main/java/com/education/controller/SysController.java
@@ -0,0 +1,100 @@
+package com.education.controller;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.education.bean.Teacher;
+import com.education.service.TeacherService;
+import com.education.untils.FinalValues;
+import com.education.untils.SHA1Util;
+
+import net.sf.json.JSONObject;
+
+
+/**
+ * 
+ * @author tianyun chen
+ * 有关登录的class，包括登录，注册
+ */
+@Controller
+@RequestMapping("api/sys")
+public class SysController {
+	@Autowired
+	TeacherService teacherService;
+	
+	
+	//TODO 老师登录
+	@RequestMapping(value="login_as_teacher",method = RequestMethod.POST)
+	public void loginAsTeacher(HttpServletRequest request,HttpServletResponse response){
+		 
+	}
+	
+	//TODO 家长登录
+	@RequestMapping(value="login_as_parent",method = RequestMethod.POST)
+	public void loginAsPartent(HttpServletRequest request,HttpServletResponse response){
+		
+	}
+	
+	//TODO 老师注册
+	@RequestMapping(value="sign_as_teacher",method = RequestMethod.POST)
+	public void signUpAsTeacher(HttpServletRequest request,HttpServletResponse response){
+        JSONObject jsonObject = new JSONObject();
+		String appSign = request.getParameter("sign");
+	    String timeStamp = request.getParameter("time_stamp");
+	    String  sign = SHA1Util.hex_sha1(timeStamp+FinalValues.PUBLIC_KEY);        
+		String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        String mobile = request.getParameter("mobile");
+        int  status = Integer.parseInt(request.getParameter("status"));
+        int classAdviser = Integer.parseInt(request.getParameter("classAdviser"));
+        Teacher tempTeacher = initTeacher(name, password, mobile, status, classAdviser);
+
+        if(!appSign.equals(sign)){
+        	jsonObject.put("code",-1);
+        }else{
+            int result = teacherService.signUpForTeacher(tempTeacher);
+        	System.out.println(result);
+            if(result==0){
+        		jsonObject.put("code",0);
+        	}else{
+        		jsonObject.put("code",1);
+        		jsonObject.put("tid",tempTeacher.getTid());
+        	}
+        }
+        try {
+			response.getWriter().write(jsonObject.toString());
+	        response.getWriter().close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//TODO 家长登录
+	@RequestMapping(value="sign_as_parent",method = RequestMethod.POST)
+	public void signUpAsParent(HttpServletRequest request,HttpServletResponse response){
+		
+	}
+	
+	
+	private Teacher initTeacher(String name,String password,String mobile,int status,int classAdviser){
+		 Teacher teacher = new Teacher();
+		 teacher.setName(name);
+		 teacher.setPassword(password);
+		 teacher.setMobile(mobile);
+		 teacher.setMobile(mobile);
+		 teacher.setStatus(status);
+		 teacher.setClassAdviser(classAdviser);
+		 return teacher;
+	}
+	
+	
+}
