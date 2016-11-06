130ac4bb7519969d0bd68c9102091ba4285d23d5
tianyun chen <tianyun chen>
2016-11-06 02:21:38 -0800

    Add: Student Controller

--- EducationSystemBackEnd/src/main/java/com/education/controller/StudentController.java
+++ EducationSystemBackEnd/src/main/java/com/education/controller/StudentController.java
@@ -1,11 +1,58 @@
 package com.education.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.education.bean.Student;
+import com.education.service.StudentService;
+
+import net.sf.json.JSONObject;
 
 @Controller
 @RequestMapping("api/student")
-public class StudentController{
-       
+public class StudentController {
 
+	@Autowired
+	StudentService studentService;
+
+	@RequestMapping(value = "createStduent", method = RequestMethod.POST)
+	public void createStudent(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		JSONObject jsonObject = new JSONObject();
+		String name = request.getParameter("name");
+		int age = Integer.parseInt(request.getParameter("age"));
+		int studentId = Integer.parseInt(request.getParameter("studentId"));
+		if (name != null && age != 0 && studentId != 0) {
+			int result = studentService.createStudent(initStudent(name, age, studentId));
+			if (result == 0) {
+				jsonObject.put("code", 0);
+			} else {
+				JSONObject dataObject = new JSONObject();
+				dataObject.put("stid", request);
+				jsonObject.put("code", 1);
+				jsonObject.put("data", dataObject);
+			}
+
+		} else {
+			jsonObject.put("code", 0);
+		}
+
+		response.getWriter().write(jsonObject.toString());
+		response.getWriter().close();
+
+	}
+
+	private Student initStudent(String name, int age, int studentId) {
+		Student student = new Student();
+		student.setName(name);
+		student.setAge(age);
+		student.setStudentId(studentId);
+		return student;
+	}
 }
--- EducationSystemBackEnd/src/main/java/com/education/service/StudentService.java
+++ EducationSystemBackEnd/src/main/java/com/education/service/StudentService.java
@@ -1,5 +1,21 @@
 package com.education.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.education.bean.Student;
+import com.education.mapper.StudentMapper;
+
+@Service
+@Transactional
 public class StudentService {
 
+	@Autowired
+	private StudentMapper studentMapper;
+	
+	public int  createStudent(Student student) {
+		studentMapper.createStudent(student);
+		return student.getStid();
+	}
 }
